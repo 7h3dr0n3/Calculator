e14333bfa040b8340454acea8600bc5aaee3575d
dr0n3 <7h3dr0n3@gmail.com>
2023-01-18 20:07:23 +0530

    remove unnecessary codes

--- app.js
+++ app.js
@@ -11,54 +11,22 @@ function divide(a, b) {
     return a / b;
 };
 
-function operate(operator, a, b) {
+function toOperate(a, operator, b) {
     switch (operator) {
-        case "add":
+        case "+":
             return add(a, b);
             break;
-        case "subtract":
+        case "-":
             return subtract(a, b);
             break;
-        case "multiply":
+        case "*":
             return multiply(a, b);
             break;
-        case "divide":
+        case "/":
             return divide(a, b);
             break;
         default:
             break;
     }
 };
-
-
-//1. if clickValue === operand/number then concat
-//2. else if clickValue === operator/+
-//then save displayedValue to numOne
-//save clickedValue to operator and concat " + " to displayedValue
-//fontSize = contWidth * maxFontSize / textWidth
-
-const display = document.getElementById('display');
-let displayValue = "", numTwo = "";
-
-const allButtons = document.querySelectorAll('button');
-allButtons.forEach(element => {
-    element.addEventListener('click', event => {
-        let clickValue = `${event.target.value}`;
-
-        if (!(event.target.classList.contains('operator'))) {
-
-            displayValue = displayValue + clickValue;
-            display.innerText = displayValue;
-        } else {
-            switch (clickValue) {
-                case "clear":
-                    displayValue = "";
-                    display.innerText = "0";
-                    break;
-                default:
-                    break;
-            };
-        }
-    });
-});
 
